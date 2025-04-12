c253da9a52a22ce72553eadb13ca5a250cde5faf
kevvo23 <kssalamea@gmail.com>
2025-04-12 20:12:31 +0200

    user access controlled

--- WeatherApp/src/main/resources/static/js/indexJS.js
+++ WeatherApp/src/main/resources/static/js/indexJS.js
@@ -8,20 +8,33 @@ buttonRegister.onclick = function(){
 buttonSubmit.onclick = function(){
     const consumerName=document.getElementById("fname").value;
     const pass=document.getElementById("fpass").value;
-    fetch("/api/getAllConsumers")
+    if(consumerName=="" || pass==""){
+        alert("Please fill in all fields.");
+        return;
+    } else{
+        fetch("/api/getAllConsumers")
         .then(response => response.json())
         .then(data => {
             console.log("Recieved Data: ", data);
             verifyLogin(data, consumerName, pass);
         })
         .catch(error => console.error("Error fetching data, "+error));
+    }
+    
 };
 
 function verifyLogin(items, consumerName, pass){
+    let found=false;
     items.forEach(item =>{
         if(item.name==consumerName && item.password==pass){
             window.open("home?id="+item.id, "_self");
+            found=true;
             return;
         }
     });
+
+    if(!found){
+        alert("Wrong username or password.");
+        location.reload(true);
+    }
 }
